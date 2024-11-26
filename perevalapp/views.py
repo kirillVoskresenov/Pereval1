ccf065318124d094848ca1c029394512641573ef
Кирилл Воскресенов <132572547+kirillVoskresenov@users.noreply.github.com>
2024-11-27 02:13:33 +0300

    Update views.py

--- perevalapp/views.py
+++ perevalapp/views.py
@@ -29,65 +29,64 @@ class PerevalAPIView(generics.CreateAPIView, generics.ListAPIView):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        coords_data = validated_data.pop('coords')
+        level_data = validated_data.pop('level')
+        image_data = validated_data.pop('image')
+
+        user_instance = User.objects.create(**user_data)
+        coords_instance = Coordinate.objects.create(**coords_data)
+        level_instance = Level.objects.create(**level_data)
+        pereval = Pereval.objects.create(**validated_data, user=user_instance, coords=coords_instance, level=level_instance)
+
+        for img_data in image_data:
+            title = img_data.pop('title')
+            image_file = img_data.pop('image')
+            Image.objects.create(pereval=pereval, title=title,
+                                 image=image_file)
+        return pereval
+
+
 class PerevalDetailAPIView(generics.RetrieveAPIView):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
+
+    def get(self, request, pk):
+        try:
+            pereval = Pereval.objects.get(pk=pk)
+            serializer = PerevalSerializer(pereval)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Pereval.DoesNotExist:
+            return Response({"message": "Запись не найдена"}, status=status.HTTP_404_NOT_FOUND)
 
 class PerevalUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
 
-class PerevalViewset(viewsets.ModelViewSet):
-    queryset = Pereval.objects.all()
-    serializer_class = PerevalSerializer
+    def patch(self, request, pk):
+        try:
+            pereval = Pereval.objects.get(pk=pk)
+            if pereval.status != 'new':
+                return Response({"state": 0, "message": "Запись не может быть изменена, так как не в статусе 'new'."},
+                                status=status.HTTP_400_BAD_REQUEST)
 
-    filterset_fields = ("user__email",)
-
-    def create(self, request, *args, **kwargs):
-        serializer = PerevalSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                'status': status.HTTP_200_OK,
-                'message': None,
-                'id': serializer.data['id'],
-            })
-        elif status.HTTP_400_BAD_REQUEST:
-            return Response({
-                'status': status.HTTP_400_BAD_REQUEST,
-                'message': 'Bad Request',
-                'id': None,
-            })
-        elif status.HTTP_500_INTERNAL_SERVER_ERROR:
-            return Response({
-                'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
-                'message': 'Ошибка подключения к базе данных',
-                'id': None,
-            })
-
-    def patch (self, request, *args, **kwargs):
-        pereval = self.get_object()
-        if pereval.status == 'New':
-            serializer = PerevalSerializer(pereval, data=request.data, partial=True)
+            serializer = PerevalSerializer(pereval, data=request.data,
+                                           partial=True)  # partial=True позволяет обновлять частично
             if serializer.is_valid():
+                user_data = serializer.validated_data.get('user', {})
+                # Проверка на редактирование запрещенных полей
+                if any(
+                        field in user_data for field in ['surname', 'name', 'patronymic', 'email', 'phone_number']
+                ):
+                    return Response({"state": 0, "message": "Эти поля нельзя редактировать"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
                 serializer.save()
-                return Response({
-                    'state': 1,
-                    'message': 'Сохранения завершены успешно'
-                })
-            else:
-                return Response({
-                    'state': 0,
-                    'message': serializer.errors
-                })
-        else:
-            return Response({
-                'state': 0,
-                'message': f"Отклонено. Причина: {pereval.get_status_display()}"
-            })
+                return Response({"state": 1, "message": "Запись успешно отредактирована."}, status=status.HTTP_200_OK)
+            return Response({"state": 0, "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Pereval.DoesNotExist:
+            return Response({"state": 0, "message": "Запись не найдена."}, status=status.HTTP_404_NOT_FOUND)
 
 
-class ImageViewset(viewsets.ModelViewSet):
-    queryset = Image.objects.all()
-    serializer_class = ImageSerializer
